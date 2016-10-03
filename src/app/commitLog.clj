8d846c3b48ce86d545beb669daf7faedb7d1e22a
StevenAvery <Steven.Avery@UOIT.net>
2016-10-03 14:00:00 -0500

    2016-10-3: 1

---
+++ src/app/commitLog.clj
@@ -0,0 +1,42 @@
+(ns app.commitLog
+  (:require [clj-time.core :as t]))
+
+;;git commit --date="Sun Jan 15 14:00 2017 -0500" -am "."
+(def numCommits `(0 1 2 4 6))
+(def logFile "commitLog.txt")
+(def logHead (str "rm -f " logFile "\ntouch " logFile "\n"))
+
+(defn commitLine
+  [date countLeft]
+    (str
+      "echo \"" (t/year date) "-" (t/month date) "-" (t/day date)
+      ": " countLeft "\" >> " logFile "\n"
+      "git commit --date=\""
+      (t/year date) "-" (t/month date) "-" (t/day date) "T14:00:00-05:00\" "
+      " -am \"" (t/year date) "-" (t/month date) "-" (t/day date)
+      ": " countLeft "\"\n"))
+
+;; start oct 2
+(defn build
+  ([pixels date]
+    (build (vec (map (fn [i] (nth numCommits i)) pixels)) date logHead))
+  ([pixels date log]
+  (if (= 0 (count pixels))
+    log
+    (if (<= (first pixels) 0)
+      ;; the day is done go to the next
+      (build
+        (into [] (drop 1 pixels))
+        (t/plus date (t/days 1))
+        log)
+      ;; take one off of the current day
+      (build
+        (assoc pixels 0 (dec (first pixels)))
+        date
+        (str log (commitLine date (first pixels)))
+        )))))
+
+(defn output
+  [filename log]
+  (spit filename log)
+  )
---
+++ src/app/imageLoad.clj
@@ -0,0 +1,42 @@
+(ns app.imageLoad
+  (:import (java.awt.image BufferedImage)
+           (javax.imageio ImageIO)
+           (java.io File)
+           (java.awt Color)))
+
+;; list of colours on github contributions graph
+(def contribColours '(
+  {:r 238 :g 238 :b 238} ;; less
+  {:r 214 :g 230 :b 133}
+  {:r 140 :g 198 :b 101}
+  {:r 68 :g 163 :b 64}
+  {:r 30 :g 104 :b 35})) ;; more
+
+(defn colourToContrib
+  "return the magnitude of contributions required for the given colour
+  or assume 0 if the colour is not found"
+  ([pixelColour] (colourToContrib pixelColour 0))
+  ([pixelColour i]
+    (cond
+      (>= i (count contribColours)) 0
+      (= (nth contribColours i) pixelColour) i
+      :else (recur pixelColour (inc i)))))
+
+(defn getPixels
+  "return all pixel colours for the given image as a single vector
+  goes top to bottom then left to right, because that is how github orders its days"
+  ([filename] (getPixels (ImageIO/read (File. "res/test.png")) 0 0 []))
+  ([image x y pixels]
+    (if (>= x (.getWidth image))
+      ;; return pixels since we've reached the end of the image
+      pixels
+      ;; otherwise keep adding to the list of pixels
+      (let [pxColourInt (Color. (.getRGB image x y))
+            pxColour {:r (.getRed pxColourInt)
+                      :g (.getGreen pxColourInt)
+                      :b (.getBlue pxColourInt)}]
+        (recur
+          image
+          (if (< y (dec (.getHeight image))) x (inc x))
+          (if (< y (dec (.getHeight image))) (inc y) 0)
+          (conj pixels (colourToContrib pxColour)))))))
