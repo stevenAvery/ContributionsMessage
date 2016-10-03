576436aac46f06f4ef9bb1b7eaffb24c40cfc168
StevenAvery <Steven.Avery@UOIT.net>
2016-10-03 14:00:00 -0500

    2016-10-3: 1

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject contributionsMessages "1.0.0"
   :description "Write messages in the github contributions section"
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-time "0.13.0"]]
   :main app.core)
--- src/app/core.clj
+++ src/app/core.clj
@@ -1,47 +1,14 @@
 (ns app.core
-  (:import (java.awt.image BufferedImage)
-           (javax.imageio ImageIO)
-           (java.io File)
-           (java.awt Color)))
+  (:require [app.imageLoad :as imageLoad]
+            [app.commitLog :as commitLog]
+            [clj-time.core :as t]))
 
-(def contribColours '(
-    {:r 238 :g 238 :b 238} ;; less
-    {:r 214 :g 230 :b 133}
-    {:r 140 :g 198 :b 101}
-    {:r 68 :g 163 :b 64}
-    {:r 30 :g 104 :b 35})) ;; more
-
-(defn colourToContrib
-  "return the magnitude of contributions required for the given colour
-  or assume 0 if the colour is not found"
-  ([pixelColour] (colourToContrib pixelColour 0))
-  ([pixelColour i]
-    (cond
-      (>= i (count contribColours)) 0
-      (= (nth contribColours i) pixelColour) i
-      :else (recur pixelColour (inc i)))))
-
-
-(defn getPixels
-  "return all pixel colours for the given image as a single vector"
-  ([image] (getPixels image 0 0 []))
-  ([image x y pixels]
-    (let [pxColourInt (Color. (.getRGB image x y))
-          pxColour {:r (.getRed pxColourInt) :g (.getGreen pxColourInt) :b (.getBlue pxColourInt)}]
-      (if (>= (inc y) (.getHeight image))
-        ;; return pixels since we've reached the end of the image
-        pixels
-        ;; otherwise keep adding to the list of pixels
-        (recur
-          image
-          (if (< x (dec (.getWidth image))) (inc x) 0)
-          (if (< x (dec (.getWidth image))) y (inc y))
-          (conj pixels (colourToContrib pxColour)))))))
-
+(def numCommits `(0 1 2 4 6))
 
 (defn -main [& args]
-  (def bi (ImageIO/read (File. "res/test.png")))
-
   ;; check the height is 7, and the width is <= 52
-  (println (getPixels bi))
-)
+  (def pixels (imageLoad/getPixels "res/test.png"))
+
+  (commitLog/output "commitLog/test.sh"
+    (commitLog/build [0 1 2 3 4] (t/date-time 2016 10 2 8 00)))
+  )
